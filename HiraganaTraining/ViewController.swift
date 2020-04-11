4f1357aa7581ac68d1ca57a023f3e661b6761199
ByunKyungMin <bkm.change.min@gmail.com>
2020-04-12 00:36:07 +0900

    Initial Commit

---
+++ HiraganaTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiraganaTraining
+//
+//  Created by 변경민 on 2020/04/12.
+//  Copyright © 2020 변경민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HiraganaTrainingTests/HiraganaTrainingTests.swift
@@ -0,0 +1,34 @@
+//
+//  HiraganaTrainingTests.swift
+//  HiraganaTrainingTests
+//
+//  Created by 변경민 on 2020/04/12.
+//  Copyright © 2020 변경민. All rights reserved.
+//
+
+import XCTest
+@testable import HiraganaTraining
+
+class HiraganaTrainingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
